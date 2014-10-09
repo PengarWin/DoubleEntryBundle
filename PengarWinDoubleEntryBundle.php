914593f95487edc72e9f0cb3185e2fae9302160e
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-10-08 22:42:30 -0400

    Initial commit

---
+++ PengarWinDoubleEntryBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the PengarWin DoubleEntryBundle package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PengarWin\DoubleEntryBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PengarWinDoubleEntryBundle extends Bundle
+{
+}
