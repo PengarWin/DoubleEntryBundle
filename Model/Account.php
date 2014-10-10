8ae9c9f18f9fb3585f9c065f32f8101382fe5dc5
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-10-09 21:14:42 -0400

    Added basic properties and accessors for Account

--- Model/Account.php
+++ Model/Account.php
@@ -9,12 +9,203 @@
 
 namespace PengarWin\DoubleEntryBundle;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 /**
  * Account
  *
  * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
  * @since  2014-10-09
+ *
+ * @ORM\MappedSuperclass
  */
-class Account
+abstract class Account
 {
+    /**
+     * id
+     */
+    protected $id;
+
+    /**
+     * postings
+     */
+    protected $postings;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    protected $balance = 0;
+
+    /**
+     * @ORM\Column(length=50, unique=false)
+     */
+    private $slug;
+
+    /**
+     * Constructor
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @param  string $name
+     */
+    public function __construct($name = null)
+    {
+        if ($name) }
+            $this->setName($name);
+        }
+
+        $this->postings = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Add posting
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @param  PostingInterface $posting
+     *
+     * @return Account
+     */
+    public function addPosting(PostingInterface $posting)
+    {
+        $this->postings->add($posting;);
+
+        return $this;
+    }
+
+    /**
+     * Remove posting
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @param  PostingInterface $posting
+     */
+    public function removePosting(PostingInterface $posting)
+    {
+        $this->postings->remove($posting;);
+    }
+
+    /**
+     * Get postings
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @return ArrayCollection|PostingInterface
+     */
+    public function getPostings()
+    {
+        return $this->postings;
+    }
+
+    /**
+     * Set name
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @param  string $name
+     *
+     * @return Account
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set balance
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @param  float $balance
+     *
+     * @return Account
+     */
+    public function setBalance($balance)
+    {
+        $this->balance = $balance;
+
+        return $this;
+    }
+
+    /**
+     * Get balance
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @return float
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+
+    /**
+     * Set slug
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @param  string $slug
+     *
+     * @return Account
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+     * @since  2014-10-09
+     *
+     * @return string
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
 }
