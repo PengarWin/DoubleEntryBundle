4b180fd869a17e958bdfee305730848bfe3b7ada
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-10-09 20:53:07 -0400

    Adding Account, Journal and Posting

---
+++ Model/Journal.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the PengarWin DoubleEntryBundle package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PengarWin\DoubleEntryBundle;
+
+/**
+ * Journal
+ *
+ * @author Tom Haskins-Vaughan <tom@harvestcloud.com>
+ * @since  2014-10-09
+ */
+class Journal
+{
+}
