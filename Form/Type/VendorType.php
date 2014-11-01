e27b2e1a74549d5a54b6211e82ff6b13ab8baf21
Tom Haskins-Vaughan <tom@templestreetmedia.com>
2014-11-01 09:46:17 -0400

    Added new VendorType

---
+++ Form/Type/VendorType.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the PengarWin DoubleEntryBundle package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PengarWin\DoubleEntryBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * VendorType
+ *
+ * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+ * @since  1.0.0
+ */
+class VendorType extends AbstractType
+{
+    /**
+     * Build the form
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('defaultOffsetAccount')
+            ->add('defaultJournalDescription')
+            ->add('defaultJournalCreditAmount')
+            ->add('defaultJournalDebitAmount')
+            ->add('save', 'submit', array('label' => $options['label']))
+            ->setAction($options['action'])
+        ;
+    }
+
+    /**
+     * Get the name for the HTTP parameter
+     *
+     * @author Tom Haskins-Vaughan <tom@tomhv.uk>
+     * @since  1.0.0
+     */
+    public function getName()
+    {
+        return 'vendor';
+    }
+}
